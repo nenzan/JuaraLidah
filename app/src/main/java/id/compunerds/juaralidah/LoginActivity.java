b88038c2a26dce91ac05be587031dd9e23a579d5
nenzan <nnurfirmansyah@gmail.com>
2020-03-06 10:12:19 +0700

    Initial commit

---
+++ app/src/main/java/id/compunerds/juaralidah/LoginActivity.java
@@ -0,0 +1,14 @@
+package id.compunerds.juaralidah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
